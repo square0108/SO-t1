5c17b82598d0bef948a5310c8fc448be81c8df7e
square0108 <martinmariollanos@gmail.com>
2025-09-07 23:14:54 -0300

    add: Constantes para configurar parsing de CLI input + Funcion dedicada para liberar la memoria de token array (deben liberarse tanto el bloque de memoria char** como la memoria de cada string insertada)

--- include/strparse.h
+++ include/strparse.h
@@ -1,10 +1,12 @@
 #include <string.h>
-#define NTOK_GUESS 64
-#define STRP_GROUP_QUOTES 0
-#define STRP_IGNORE_QUOTES 1
+#define STRP_QUOTE_HANDLING_OFF 0
+#define STRP_QUOTE_HANDLING_ON 1
+#define STRP_REMOVE_LAST_NEWLINE_OFF 0
+#define STRP_REMOVE_LAST_NEWLINE_ON 1
 
 /* !! HEAP RETURN VALUE !! PLEASE FREE() HIM.  
 Tokeniza la string `str`, separando tokens segun el string delimitador `delim` y retorna un arreglo NULL-terminado de los tokens extraidos.
 */
-char** partition_into_array(char* str, char* delim);
+char** partition_into_array(char* str, _Bool quote_handling, _Bool del_last_newline);
+void free_tokenized_array(char** tokarray);
 
